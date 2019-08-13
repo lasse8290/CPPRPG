e21d8944da647517c1a2a19c05071c760696269c
Sean <sean0262@gmail.com>
2019-08-13 14:15:57 +0200

    14:15

--- CPPRPG/Items.h
+++ CPPRPG/Items.h
@@ -7,12 +7,14 @@ class Item
 
 
         public:
-          Item(const std::string& name, int id)
-           : name(name), itemId(id) { }
+          Item(const std::string& name, int id, std::string& description)
+           : name(name), itemId(id), desc(description) { };
           const string& getName(void) const { return this->name; }
           int getId(void) const { return this->itemId; }
+          const string& getDesc(void) const { return this->description; }
         private:
           string name;
           int itemId;
+          string description;
 
 }
