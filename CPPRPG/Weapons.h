e25c089c196c9883a2776a65e8cfc3c90d54e2e8
Sean <sean0262@gmail.com>
2019-08-13 13:25:48 +0200

    13/08/19

--- CPPRPG/Weapons.h
+++ CPPRPG/Weapons.h
@@ -9,19 +9,15 @@ class Weapons
 
 
       public:
-
-        Weapons(const std::string& name, int id, int type)
-         : weapName(name), weapId(id), weapType(type) { }
-
           //Available weapon name.
-          const string& weapName(void) const { return this->name; }
+          Weapon(const std::string& name, int id)
+           : Item(name, id) { }
 
 
           //Weapon Stats.
           virtual int dur(void) const = 0, maxDur(void) const = 0, physDmg(void) const = 0, magDmg(void) const = 0, fireDmg(void) const = 0, poisDmg(void) const = 0;
           virtual int lvlReq, hpReq, mpReq, weapType, weapId;
           virtual int lifeSteal(void) const = 0, attackSpeed(void) const = 0;
-
 
 
 }
