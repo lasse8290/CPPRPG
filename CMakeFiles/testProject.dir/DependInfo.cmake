06b5ee24e05a889f1808457ec43b3d3e5e0a808c
lasse <lassefj0@gmail.com>
2019-08-18 14:12:45 +0200

    more cmake shit

---
+++ CMakeFiles/testProject.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Git-Clones/Sean Spagt/CPPRPG/Character.cpp" "E:/Git-Clones/Sean Spagt/CMakeFiles/testProject.dir/CPPRPG/Character.cpp.obj"
+  "E:/Git-Clones/Sean Spagt/CPPRPG/Inventory.cpp" "E:/Git-Clones/Sean Spagt/CMakeFiles/testProject.dir/CPPRPG/Inventory.cpp.obj"
+  "E:/Git-Clones/Sean Spagt/CPPRPG/Item.cpp" "E:/Git-Clones/Sean Spagt/CMakeFiles/testProject.dir/CPPRPG/Item.cpp.obj"
+  "E:/Git-Clones/Sean Spagt/CPPRPG/Main.cpp" "E:/Git-Clones/Sean Spagt/CMakeFiles/testProject.dir/CPPRPG/Main.cpp.obj"
+  "E:/Git-Clones/Sean Spagt/CPPRPG/Weapon.cpp" "E:/Git-Clones/Sean Spagt/CMakeFiles/testProject.dir/CPPRPG/Weapon.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/testProject.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "Builds/bin/testProject.exe"
+  "Builds/bin/testProject.exe.manifest"
+  "Builds/bin/testProject.pdb"
+  "Builds/lib/libtestProject.dll.a"
+  "CMakeFiles/testProject.dir/CPPRPG/Character.cpp.obj"
+  "CMakeFiles/testProject.dir/CPPRPG/Inventory.cpp.obj"
+  "CMakeFiles/testProject.dir/CPPRPG/Item.cpp.obj"
+  "CMakeFiles/testProject.dir/CPPRPG/Main.cpp.obj"
+  "CMakeFiles/testProject.dir/CPPRPG/Weapon.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
